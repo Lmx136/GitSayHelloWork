7fd2792d6144494a284f9dc8d905def69ebc6a58
1506108744 <1506108744@qq.com>
2020-08-17 18:44:20 +0800

    1

--- GitSayHelloWork/GitSayHelloWork/Form1.cs
+++ GitSayHelloWork/GitSayHelloWork/Form1.cs
@@ -19,7 +19,7 @@ namespace GitSayHelloWork
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("欢迎来到我的git");
+            MessageBox.Show("欢迎来到我的git11111111");
         }
     }
 }
